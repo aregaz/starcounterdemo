ebb6e1aecfb7ff599dd479ebcc27de884db8ab1a
Illia Ratkevych <illia.ratkevych@gmail.com>
2017-06-10 22:52:39 +0300

    Commit after system crash

--- RealEstateFranchiseAgency/RealEstateFranchiseAgency/Program.cs
+++ RealEstateFranchiseAgency/RealEstateFranchiseAgency/Program.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using Starcounter;
+﻿using Starcounter;
+using RealEstateFranchiseAgency.Database;
 
 namespace RealEstateFranchiseAgency
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
+            Application.Current.Use(new HtmlFromJsonProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
 
+            Handle.GET("/RealEstateFranchiseAgency", () =>
+            {
+                var corporation = Db.SQL<Corporation>("select c from Corporation").First;
+
+                var json = new CorporationJson
+                {
+                    Data = corporation
+                };
+
+                if (Session.Current == null)
+                {
+                    Session.Current = new Session(SessionOptions.PatchVersioning);
+                }
+
+                json.Session = Session.Current;
+                return json;
+            });
         }
     }
 }
