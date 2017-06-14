342e71521a4b130e261673e7dde49f3c0d384e57
Illia Ratkevych <illia.ratkevych@gmail.com>
2017-06-14 22:31:08 +0300

    Add office details components

---
+++ src/RealEstateAgencyFranchise/Controllers/OfficeController.cs
@@ -0,0 +1,13 @@
+﻿using Starcounter;
+using System;
+
+namespace RealEstateAgencyFranchise.Controllers
+{
+    internal class OfficeController
+    {
+        public Json Get(long officeObjectNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
