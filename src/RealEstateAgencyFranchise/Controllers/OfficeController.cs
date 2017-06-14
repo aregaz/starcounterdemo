f0d28e48335c102ce626fae6b6d76e6188881276
Illia Ratkevych <illia.ratkevych@gmail.com>
2017-06-14 23:24:03 +0300

    Implement getting data for office details

--- src/RealEstateAgencyFranchise/Controllers/OfficeController.cs
+++ src/RealEstateAgencyFranchise/Controllers/OfficeController.cs
@@ -1,13 +1,42 @@
-﻿using Starcounter;
-using System;
+﻿using RealEstateAgencyFranchise.Database;
+using Starcounter;
+using System.Linq;
 
 namespace RealEstateAgencyFranchise.Controllers
 {
     internal class OfficeController
     {
-        public Json Get(long officeObjectNo)
+        public Json Get(ulong officeObjectNo)
         {
-            throw new NotImplementedException();
+            return Db.Scope(() =>
+            {
+                var offices = Db.SQL<Office>(
+                    "select o from Office o where o.ObjectNo = ?",
+                    officeObjectNo);
+                if (offices == null || !offices.Any())
+                {
+                    return new Response()
+                    {
+                        StatusCode = 404,
+                        StatusDescription = "Office not found"
+                    };
+                }
+
+                var office = offices.First;
+
+                var json = new OfficeListJson
+                {
+                    Data = office
+                };
+
+                if (Session.Current == null)
+                {
+                    Session.Current = new Session(SessionOptions.PatchVersioning);
+                }
+
+                json.Session = Session.Current;
+                return json;
+            });
         }
     }
 }
